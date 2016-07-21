de4c56e08714c63dec8e803961a80b4bfcec1dfc
danielkermode <danielkermode@hotmail.co.nz>
2016-07-21 18:32:45 +1200

    rspec

---
+++ spec/requests/welcome_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+
+# RSpec.describe "Welcome", type: :request do
+#   describe "GET /" do
+#     it "works! (now write some real specs)" do
+#       get :index
+#       expect(response).to render_template("index")
+#     end
+#   end
+# end
---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "users/index", type: :view do
+  before(:each) do
+    assign(:users, [
+      User.create!(username: 'asd', password: 'asddd'),
+      User.create!(username: 'asd', password: 'asddd')
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+  end
+end
